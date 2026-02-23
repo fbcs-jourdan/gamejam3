e8a498fa5db5429d20dc653a5d7ef44cb72692bc
fbcs-jourdan <2260504@flourbluffschools.org>
2026-02-23 12:37:16 -0600

    add working streetlight and speedometer

---
+++ night-racing/scripts/road_tiles.gd
@@ -0,0 +1,30 @@
+extends GridMap
+
+var light = preload("res://light_curved_2.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# find all of the lights using the light-curved-2 mesh
+	var lights := get_used_cells_by_item(56)
+	
+	# replace all occurences of the light-curved-2 mesh
+	# with instances of the light_curved_2 scene
+	for pos in lights:	
+		var world_pos = to_global(map_to_local(pos))
+		var new_light := light.instantiate()
+		add_child(new_light)
+		new_light.global_position = world_pos
+		
+		# get the direction the light is facing
+		# this I hard coded, not sure how to tell direction any other way
+		# if you see it printing, add another elif and figure out the rotation angle
+		var orientation = get_cell_item_orientation(pos)
+		if orientation == 0:
+			new_light.rotation.y = 0
+		elif orientation == 22:
+			new_light.rotation.y = -90
+		elif orientation == 24:
+			new_light.rotation.y = -90
+		else:
+			print('open road_tiles.gd and add orientation: ' +str(orientation))
+		
+		set_cell_item(pos, -1)
--- night-racing/speedometer.gd
+++ night-racing/speedometer.gd
@@ -1,7 +1,8 @@
 extends Control
 
 @onready var label: Label = $Label
+@onready var texture_rect_2: TextureRect = $TextureRect2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	label.text = str(SPEED.velocity)
+	texture_rect_2.rotation = (SPEED.velocity * 10.5) * delta
